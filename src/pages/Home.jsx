f46e558d24070bd441c0dd1d5ea84f1eff42f901
Arko <arkotesla@gmail.com>
2024-12-04 01:22:28 +0600

    fourth commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+import Lastsec from '../components/Lastsec'
+import Fivesec from '../components/FIvesec'
+import Foursec from '../components/foursec'
+import HeroSec from '../components/HeroSec'
+import CardSec from '../components/CardSec'
+import Thirdsec from '../components/ThirdSec'
+import Secondsec from '../components/SecondSec'
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <NavBar/>
+     <HeroSec/>
+     <Secondsec/>
+     <CardSec/>
+     <Thirdsec/>
+     <Foursec/>
+     <Fivesec/>
+     <Lastsec/>
+     <Footer/>
+    </>
+  )
+}
+
+export default Home
